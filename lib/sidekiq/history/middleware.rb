d4db15c8bd1180f21a53b1bdc07270f9db61977d
Oleg Makarov <gstancox@gmail.com>
2014-10-13 19:11:25 +0300

    add finished at

--- lib/sidekiq/history/middleware.rb
+++ lib/sidekiq/history/middleware.rb
@@ -9,21 +9,28 @@ module Sidekiq
         self.msg = msg
 
         data = {
-          started_at: Time.now.utc,
+          started_at: Time.now,
           payload: msg,
           worker: msg['class'],
           processor: "#{identity}-#{Thread.current.object_id}",
-          queue: queue
+          queue: queue,
+          finished_at: nil
         }
-
-        Sidekiq.redis do |conn|
+        
+        
+        index = Sidekiq.redis do |conn|
           conn.lpush(LIST_KEY, Sidekiq.dump_json(data))
-          unless Sidekiq.history_max_count == false
-            conn.ltrim(LIST_KEY, 0, Sidekiq.history_max_count - 1)
-          end
+          #unless Sidekiq.history_max_count == false
+            #conn.ltrim(LIST_KEY, 0, Sidekiq.history_max_count - 1)
+          #end
         end
 
         yield
+        
+        data[:finished_at] = Time.now
+        Sidekiq.redis do |conn|
+          conn.lset(LIST_KEY, -index, Sidekiq.dump_json(data))
+        end
       end
     end
   end
